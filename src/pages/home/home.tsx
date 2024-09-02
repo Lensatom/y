836431c41053d365ffe7a2ffce6842ad5eb66a82
Lensatom <ayomidetommiwa@gmail.com>
2024-09-02 09:50:52 +0100

    initial commit

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/base"
+
+const Home = () => {
+  return (
+    <div className="h-screen w-full bg-background">
+      <Button>Hello</Button>
+      Home
+    </div>
+  )
+}
+
+export default Home
